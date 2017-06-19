55368efcd5a2eae64abd519cc6ea3a33af41515b
Chris <cmeyerhoff@users.noreply.github.com>
2017-06-19 18:05:29 -0400

    Working messages part 3, but synchonrous I think (false flag and no response handler, but functional)

--- messages/lab.js
+++ messages/lab.js
@@ -3,7 +3,7 @@
 function parse(){
 
 	var XMLrequest = new XMLHttpRequest();
-	XMLrequest.open("GET", "data.json", false);
+	XMLrequest.open("GET", "https://messagehub.herokuapp.com/messages.json", false);
 	XMLrequest.send(null);
 
 	var reponse = XMLrequest.responseText;
@@ -17,8 +17,8 @@ function parse(){
 
 }
 
-/* parsedData = JSON.parse(jsonData);
-      messages = document.getElementById("messages");
-      for (i = 0; i <parsedData.length; i++) {
-        messages.innerHTML += parsedData[i]['content'] + " " + parsedData[i]['username'] + "<br>";
-      }  */
+
+
+// part 2 does not work, trying to attempt a cross origin request. When creating a simple server, the document is
+// within the server/on the same domain. When trying with opening index.html, the file is treated as a local file and
+// not within the server
