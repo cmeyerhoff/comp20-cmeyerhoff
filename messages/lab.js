3c416a36aec5fd6a0c5d301f6c693e4a3a28de8d
Chris <cmeyerhoff@users.noreply.github.com>
2017-06-19 17:57:47 -0400

    Working messages part 1, one warning though

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,24 @@
 // Your JavaScript goes here...
+
+function parse(){
+
+	var XMLrequest = new XMLHttpRequest();
+	XMLrequest.open("GET", "data.json", false);
+	XMLrequest.send(null);
+
+	var reponse = XMLrequest.responseText;
+	parsedData = JSON.parse(reponse);
+	console.log(reponse);
+	console.log(parsedData[0]['content']);
+	messages = document.getElementById("messages");
+      for (i = 0; i <parsedData.length; i++) {
+        messages.innerHTML += parsedData[i]['content'] + " " + parsedData[i]['username'] + "<br>";
+      } 
+
+}
+
+/* parsedData = JSON.parse(jsonData);
+      messages = document.getElementById("messages");
+      for (i = 0; i <parsedData.length; i++) {
+        messages.innerHTML += parsedData[i]['content'] + " " + parsedData[i]['username'] + "<br>";
+      }  */
